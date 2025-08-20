cd2a959da3ef9b8b25bf299524fed3aa1b54fd96
ksmindpks <ksparkp@gmail.com>
2025-08-20 11:11:37 +0900

    april20am

--- source/12_django/myproject/article/views.py
+++ source/12_django/myproject/article/views.py
@@ -5,15 +5,14 @@ from article.models import Article
 from django.urls import reverse_lazy
 from django.core.paginator import Paginator
 # /article/ : 1페이지 /article/?page=2 : 2페이지
-# def article_list(request):
-#     article_list = Article.objects.all()
-# 	paginator = Paginator(article_list, 3)
-# 	page_number = request.GET.get('page', '1')
-# 	page_object = paginator.get_page(page_number)
-# 	return render(request,
-#                 'article/article_list.html',
-#                 {'article_list': page_object,
-# 				'page_obj':page_object})
+def article_list(request):
+	article_list = Article.objects.all()
+	paginator = Paginator(article_list, 3)
+	page_number = request.GET.get('page', '1')
+	page_object = paginator.get_page(page_number)
+	return render(request, 'article/article_list.html',
+                {'article_list': page_object,
+				'page_obj':page_object})
 
 article_list = ListView.as_view(model=Article, 
 								# template_name="article/article_list.html",
--- source/12_django/myproject/myproject/urls.py
+++ source/12_django/myproject/myproject/urls.py
@@ -26,3 +26,8 @@ urlpatterns = [
     path("book/", include("book.urls")),
     path("article/", include("article.urls")),
     ]
+# 장고는 static 파일은 자동 연결해 주나, media 파일은 개발자가 url과 저장경로를 연결
+from django.conf.urls.static import static
+from . import settings
+urlpatterns += static(settings.MEDIA_URL, # "/media/~~"
+                      document_root=settings.MEDIA_ROOT) # BASE_DIR/_media 저장
