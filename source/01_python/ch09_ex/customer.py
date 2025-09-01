68ebbec2f90bb936a33a60895e014290b6b14680
ksmindpks <ksparkp@gmail.com>
2025-09-01 10:57:48 +0900

    sep01am

--- source/01_python/ch09_ex/customer.py
+++ source/01_python/ch09_ex/customer.py
@@ -1,83 +1,62 @@
-# customer.py = Customer클래스, to_customer(), load_customer()
+# customer.py = Customer클래스 to_customer(), load_customer()
 class Customer:
-    '고객데이터와 as_dict(), to_list_style (txt 백업시 필요), __str__()'
+    '고객데이터와 as_dic(), to_list_style(txt백업시), __str__()'
     def __init__(self, name, phone, email, age, grade, etc):
         self.name = name
         self.phone = phone
         self.email = email
-        self.age = age
+        self.age  = age
         self.grade = grade
         self.etc = etc
-        pass
-        
-    def as_dict(self): # 객체를 딕셔너리데이터로 반환 (csv 파일 저장시)
-        return {
-            'name':self.name,
-            'phone':self.phone,
-            'email':self.email,
-            'age':self.age,
-            'grade':self.grade,
-            'etc':self.etc
-            }
+    def as_dic(self):
+        return {"name":self.name, 
+                "phone":self.phone, 
+                "email":self.email,
+                "age":self.age, 
+                "grade":self.grade, 
+                "etc":self.etc}
+    def to_list_style(self):
+        # return "홍길동, 010-9999-9999, a@a.com, 30, 3, 까칠해"
+#         return "{}, {}, {}, {}, {}, {}".format(self.name, self.phone, self.email,
+#                                               self.age, self.grade, self.etc)
+        temp = [self.name, 
+                self.phone, 
+                self.email, 
+                str(self.age), 
+                str(self.grade), 
+                self.etc]
+        return ', '.join(temp)
+    def __str__(self):
+        # return "  *** 홍길동, 010-9999-9999, a@a.com, 30, 까칠해"
+        return "{:>5}\t{:3}\t{:13}\t{:15}\t{:3}\t{}".format('*'*self.grade,
+                       self.name, self.phone, self.email, self.age, self.etc)
     
-    def to_list_style(self): #객체를 list return('홍길동, 010-8999-9999, e@e.com, 20, 3, 까칠해'), format이나 join함수 사용
-        return '{}, {}, {}, {}, {}, {} '.format(
-            self.name,
-            self.phone,
-            self.email,
-            int(self.age),
-            int(self.grade),
-            self.etc)
-#         temp = [self.name,
-#             self.phone,
-#             self.email,
-#             str(self.age),
-#             str(self.grade),
-#             self.etc]
-#         return ', '.join(temp)
-         
-    def __str__(self): #  return '***홍길동 010-8999-9999 e@e.com 20 까칠해'
-        return '{:>5}\t{:3}\t{:13}\t{:10}\t{:3}\t{}'.format('*'*self.grade,
-                                          self.name,
-                                          self.phone,
-                                          self.email,
-                                          self.age,
-                                          self.etc)
-
 def to_customer(row):
     '''
-        txt파일 내용 한줄 row ='홍길동 , 010-8999-9999, e@e.com, 20, 3, 까칠해' (txt파일 내용)을 
-        매개변수로 받아 Customer 객체로 반환
+    row = "홍길동, 010-9999-9999, a@a.com, 30, 3, 까칠해"(txt파일 내용)을 매개변수로 받아
+    Customer 객체로 return 
     '''
     data = row.split(',')
-
     name = data[0]
-    phone = data[1].strip()
+    phone = data[1].strip() # strip은 앞뒤 white space 제거
     email = data[2].strip()
-    age = int(data[3].strip()) # strip() : 좌우 공백(space, \t, \n) 제거
+    age = int(data[3].strip())
     grade = int(data[4].strip())
     etc = data[5].strip()
     return Customer(name, phone, email, age, grade, etc)
 
-# 0.실행하면 data/ch09_customers.txt 파일의 내용을 load(customer_list)
-#      data/ch09_customers.txt이 존재하지 않으면
-#          빈 data/ch09_customers.txt 파일을 생성하고
-#              데이터는 customer_list=[]
-
 def load_customers():
     customer_list = []
     try:
-        with open('data/ch09_customers.txt', 'r', encoding='utf-8') as txt_f:
-            # txt 데이터 한줄씩 customer 객체로 받아 customer.append
-            lines = txt_f.readlines()
+        with open('data/ch09_customers.txt', 'r', encoding='utf-8') as f:
+            # txt 데이터 한줄씩 customer 객체로 받아 customer_list.append
+            lines = f.readlines()
             for line in lines:
-                # line = '홍길동, 010-8999-9999, e@e.com, 30, 3, 까칠해'
+                #line = "홍길동, 010-9999-9999, a@a.com, 30, 3, 까칠해"
                 customer = to_customer(line)
                 # print(customer)
                 customer_list.append(customer)
-            print('메모리에 upload 되었습니다.')
-
-    except FileNotFoundError:
-        with open('data/ch09_customers.txt', 'w', encoding='utf-8') as txt_f:
+    except:
+        with open('data/ch09_customers.txt', 'w', encoding='utf-8') as f:
             print('초기화 파일을 생성했습니다')
     return customer_list
--- source/01_python/ch09_ex/example.py
+++ source/01_python/ch09_ex/example.py
@@ -1,37 +1,34 @@
-#
 from customer import load_customers
 from functions import fn1_insert_customer_info, fn2_print_customers
-from functions import fn3_delete_customer, fn4_search_customer, fn5_save_customer_csv, fn9_save_customer_txt
+from functions import fn3_delete_customer, fn4_search_customer, fn5_save_customer_csv
+from functions import fn9_save_customer_txt
 
 def main():
-    # global customer_list
-    customer_list = load_customers() # ch09_customers.txt의 내용을 load
+    global customer_list
+    customer_list = load_customers()
     while True:
-        print("1:입력 ","2:전체출력 ","3:삭제 ","4:이름찾기 ","5:내보내기 (CSV)", "9:종료 ", sep=' | ', end='')
-        fn = input('메뉴선택):')
-        if fn == '1':
-            customer = fn1_insert_customer_info() # 입력받은 내용으로 customer 객체를 반환
+        print("1:입력|2:전체출력|3:삭제|4:이름검색|5:csv내보내기|9: 종료", end='')
+        fn = input("메뉴 선택 : ")
+        if fn=='1':
+            # print("fn1_호출한 결과를 customer에 받아 customer_list에 append")
+            customer = fn1_insert_customer_info()
             customer_list.append(customer)
-            print('fn1_호출한 결과를 customer에 받아 customer_list에 append')
-        elif fn == '2':
+        elif fn=='2':
+            # print('fn2_호출')
             fn2_print_customers(customer_list)
-            print('fn2_호출한 결과, 전체 출력')
-        elif fn == '3':
+        elif fn=='3':
+            # print('fn3_호출')
             fn3_delete_customer(customer_list)
-            print('fn3_호출한 결과, 항목 삭제')
-        elif fn == '4':
+        elif fn=='4':
+            # print('fn4_호출')
             fn4_search_customer(customer_list)
-            print('fn4_호출한 결과, 검색 항목 출력')
-        elif fn == '5':
+        elif fn=='5':
+            # print('fn5_호출')
             fn5_save_customer_csv(customer_list)
-            print('fn5_호출한 결과, 지정한 이름으로 csv 파일 생성')
-        elif fn == '9':
+        elif fn=='9':
+            # print('fn9_호출')
             fn9_save_customer_txt(customer_list)
-            print('fn2_호출한 결과, 백업파일(.txt) 저장 후 종료')
             break
-        else:
-            print('올바른 메뉴 번호를 선택 바랍니다.')
 
-# main() 함수 호출
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
